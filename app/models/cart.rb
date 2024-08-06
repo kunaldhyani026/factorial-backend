0f5040c1a15cc77fa5f05ecde2d6e0be3a7487a2
Kunal <kunaldhyani026@gmail.com>
2024-08-06 18:24:40 +0530

    Factorial Backend : total_price method restructured

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,21 +7,6 @@ class Cart < ApplicationRecord
 
   # Method to calculate the total price of the cart
   def total_price
-    cart_items.sum do |cart_item|
-      cart_item_total_price(cart_item)
-    end
-  end
-
-  private
-
-  # Helper method to calculate the total price of a specific cart item
-  def cart_item_total_price(cart_item)
-    # Sum the prices of all customizable options associated with the cart item
-    price = 0
-    cart_item_customizable_options = cart_item.customizable_options
-    cart_item_customizable_options.each do |cart_item_customizable_option|
-      price += cart_item_customizable_option.actual_price(cart_item_customizable_options)
-    end
-    price
+    cart_items.sum(&:total_price)
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,13 @@ class CartItem < ApplicationRecord
   belongs_to :product, validate: true
 
   has_and_belongs_to_many :customizable_options
+
+  # Sum the prices of all customizable options associated with the cart item
+  def total_price
+    price = 0
+    customizable_options.each do |customizable_option|
+      price += customizable_option.actual_price(customizable_options)
+    end
+    price
+  end
 end
